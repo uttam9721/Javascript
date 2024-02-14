252863a2abdf1f6360d652a8961376a10e2a699b
Uttam Kumar <uttammaurya377@gmail.com>
2024-02-14 23:18:15 +0530

    use to update because use all method

--- MethodChaining.js
+++ MethodChaining.js
@@ -8,6 +8,22 @@ let val1 = str.toLowerCase();
 console.log(val1);
 
 //method chaining use to perform multiple task in the cose 
-let str1 = "  ILoveCoding"    ;
-let val2 = str1 = str1.toUpperCase().trim();//trim() use to reduce the white space left or right in the string except to the middle  
+let val2 = str.toUpperCase().trim();//trim() use to reduce the white space left or right in the string except to the middle  
 console.log(val2);
+
+
+//use another method 
+//slice to cut in the part of the string
+let val4 = str.slice(1,5);
+console.log(val4); //Love 
+
+
+//  replace(); it replace the string in to another string 
+let val6 =str.replace("Love","Do");
+console.log(val6);
+
+
+
+  //repeat() use to repeat again and again string 
+  let str7 = str.repeat(3);
+  console.log(str7);//ILoveCodingILoveCodingILoveCoding
