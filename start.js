914ff9cc3162717c4e6c5b18525f868f6eb13180
Uttam Kumar <uttammaurya377@gmail.com>
2024-02-14 14:55:03 +0530

    Add js file

---
+++ start.js
@@ -0,0 +1,5 @@
+let newBtn = document.createElement("button");
+newBtn.innerText="click me!";
+newBtn.style.color="white";
+newBtn.style.backgroundColor="red";
+document.querySelector("body").prepend(newBtn);
