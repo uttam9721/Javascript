33053288368047ef742d5a5b79844a5dad50021c
Uttam Kumar <uttammaurya377@gmail.com>
2024-02-23 00:52:21 +0530

    Arrow function

---
+++ ArrowFunction.js
@@ -0,0 +1,34 @@
+// Arrow function is a updated javascript version 
+const sum = (a,b)=>{
+    console.log(a+b);
+}
+sum(2,3);//output is 5
+
+
+const cube=(n)=>{
+    return n*n*n;
+}
+console.log(cube(5));// output is 125;
+
+
+
+const pow =(a,b)=>{
+    return a**b;
+}
+console.log(pow(4,2))//output is 16 because 4 raised to the power of
+
+
+
+
+
+//using setTimeout in javascript 
+setTimeout(()=>{
+    console.log("uttam maurya ");
+},2000);//print the value after 2 second 
+
+
+
+//using setInterval in javascript 
+setInterval(()=>{
+  console.log("uttam maurya ");
+},2000);//it display the output again and again  after every 2 second 
