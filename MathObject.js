c20f0f3d030c7e774a3b6f980fd27199263ae74d
Uttam Kumar <uttammaurya377@gmail.com>
2024-02-23 00:25:19 +0530

    try and catch to reduce error in out code

---
+++ MathObject.js
@@ -0,0 +1,16 @@
+//Using try and catch in javascript
+//A block of code that may cause an error is enclosed within a try statement.
+try{
+    console.log(a);
+}catch{
+    console.log("variable does't  exist");//variable does't  exist
+}
+
+
+// using take a variable 
+let a =5;
+try{
+    console.log(a);
+}catch{
+    console.log("variable does't  exist");//5
+}
