2d026093806877e1031b2c6a08548b1a31786b0c
Uttam Kumar <uttammaurya377@gmail.com>
2024-02-23 00:29:46 +0530

    math function

--- MathObject.js
+++ MathObject.js
@@ -1,16 +1,11 @@
-//Using try and catch in javascript
-//A block of code that may cause an error is enclosed within a try statement.
-try{
-    console.log(a);
-}catch{
-    console.log("variable does't  exist");//variable does't  exist
-}
-
-
-// using take a variable 
-let a =5;
-try{
-    console.log(a);
-}catch{
-    console.log("variable does't  exist");//5
-}
+//Math.abs(n)
+//Math.pow(a,b)
+//Math.floor(n)
+//Math.ceil(n)
+//Math.random(n)=> use to generate a random number
+let num1 = 9;
+console.log("The absolute value of " + num1 + " is: "+ Math.abs);
+console.log(Math.pow(2,3));//output = > 8
+console.log(Math.floor(2.3));//output = > 2
+console.log(Math.ceil(2.3));//output = > 3
+console.log(Math.random());//output = > random value for eg: 0.2703147387113858 
